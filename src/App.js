4af9eb08e3c6b410c677edc214e1c8d7d4a00bac
AnsonLoHK <chiaochiao@gmail.com>
2022-07-27 18:17:57 +0800

    add git repo link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <div className="fl w-100 w-third-ns pa2">
         <ShortContent />
         <div className="mt2">
-          <a href="" className="green">
+          <a
+            href="https://github.com/AnsonLoHK/reactjs-popup-window-ad"
+            className="green"
+          >
             代碼網址
           </a>
         </div>
